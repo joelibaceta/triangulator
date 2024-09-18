d38cc69d5f02542db440675c5b2d4bc7ff25ff38
Joel Ibaceta <mail@joelibaceta.com>
2024-09-18 11:13:28 -0500

    Fix triangulation issues: ensure correct winding order and improve point-in-triangle check using barycentric coordinates

--- triangulator/ear_clipping_method.py
+++ triangulator/ear_clipping_method.py
@@ -1,82 +1,76 @@
 from triangulator.geometry.angles import get_polygon_internal_angle
-from triangulator.geometry.points import is_point_inside_triangle
+from triangulator.geometry.points import is_point_inside_triangle_barycentric
 
-def triangulate(poligon: tuple) -> list:
+def signed_area(polygon):
+    """Calculate the signed area of a polygon to determine its winding order."""
+    area = 0
+    for i in range(len(polygon)):
+        x1, y1 = polygon[i]
+        x2, y2 = polygon[(i + 1) % len(polygon)]
+        area += (x1 * y2 - x2 * y1)
+    return area / 2
+
+def ensure_winding_order(polygon):
+    """Ensure that the polygon vertices are in counter-clockwise order."""
+    if signed_area(polygon) < 0:
+        polygon.reverse()  # Reverse if the order is clockwise
+    return polygon
+
+def triangulate(polygon: tuple) -> list:
     """
-    This function will triangulate any polygon and return a list of triangles
+    This function will triangulate any polygon and return a list of triangles.
 
         Parameters: 
-            poligon (tuple): A tuple of tuples with the points of the polygon
+            polygon (tuple): A tuple of tuples with the points of the polygon
         Returns:
             list: A list of tuples with the triangles vertices
-
-    """
-
-
-    """
-    Explanation:
-    ============
-
-        This function will triangulate any polygon and return a list of triangles
-        The algorithm is based on the ear clipping method, which is a simple method
-
-    Algorithm:
-    ==========
-
-        1. Find the internal angle of each vertex
-        2. Check if the angle is less than 180 degrees, skip if not
-        3. Check if there are any polygon points inside the triangle formed by the vertex and its neighbors
-        4. If there are no points inside the triangle, then the vertex is an ear
-        5. If the vertex is an ear, add the triangle to the list of triangles
-        6. Remove the ear and repeat the process until there are no more ears
-
-    References:
-    ===========
-        https://en.wikipedia.org/wiki/Polygon_triangulation#Ear_clipping_method
-        https://www.youtube.com/watch?v=QAdfkylpYwc
     """
 
     final_triangles = []
-    vertices = list(poligon)
-    original_vertices = list(poligon)
-    triangles_finded = -1
-    
+    vertices = list(polygon)
+    original_vertices = list(polygon)
+    triangles_found = -1
+
+    # Ensure the winding order is counter-clockwise
+    vertices = ensure_winding_order(vertices)
+
     # While there are triangles to be found
-    while(triangles_finded != 0): 
-      triangles_finded = 0
+    while triangles_found != 0: 
+        triangles_found = 0
 
-      for index, _ in enumerate(vertices):
-        prev_vertice = vertices[index - 1]
-        next_vertice = vertices[(index + 1) % (len(vertices))] # using mod to avoid index out of range
-        vertice = vertices[index]
+        for index, _ in enumerate(vertices):
+            prev_vertex = vertices[index - 1]
+            next_vertex = vertices[(index + 1) % len(vertices)]  # using mod to avoid index out of range
+            vertex = vertices[index]
 
-        # Get Vector from prev_vertice to vertice
-        vector1 = (vertice[0] - prev_vertice[0], vertice[1] - prev_vertice[1])
-        # Get Vector from vertice to next_vertice
-        vector2 = (next_vertice[0] - vertice[0], next_vertice[1] - vertice[1])
-        # Get internal angle
-        angle = get_polygon_internal_angle(vector1, vector2)
+            # Get Vector from prev_vertex to vertex
+            vector1 = (vertex[0] - prev_vertex[0], vertex[1] - prev_vertex[1])
+            # Get Vector from vertex to next_vertex
+            vector2 = (next_vertex[0] - vertex[0], next_vertex[1] - vertex[1])
+            # Get internal angle
+            angle = get_polygon_internal_angle(vector1, vector2)
 
-        if angle >= 180:
-            # Skip because angle is greater than 180
-            continue
-        else:
-            # Build a triangle with the three vertices
-            triangle = (prev_vertice, vertice, next_vertice)
-            # Get vertices that are not part of the triangle
-            points = [p for p in original_vertices if p not in triangle]
-            # Check if there is a vertice inside the triangle
-            inside_evaluation = [is_point_inside_triangle(triangle, point) for point in points]
-            # If are not points inside the triangle
-            if True in inside_evaluation:
-                # Skip because point is inside triangle
+            if angle >= 180:
+                # Skip because angle is greater than or equal to 180
                 continue
             else:
-                # Add triangle to final triangles
-                final_triangles.append(triangle)
-                # Remove vertice from vertices
-                vertices.pop(index)
-                # Increment triangles finded
-                triangles_finded += 1
-                break
+                # Build a triangle with the three vertices
+                triangle = (prev_vertex, vertex, next_vertex)
+                # Get vertices that are not part of the triangle
+                points = [p for p in original_vertices if p not in triangle]
+                # Check if there is a vertex inside the triangle using barycentric coordinates
+                inside_evaluation = [is_point_inside_triangle_barycentric(triangle, point) for point in points]
+                # If no points are inside the triangle
+                if True in inside_evaluation:
+                    # Skip because a point is inside the triangle
+                    continue
+                else:
+                    # Add triangle to final triangles
+                    final_triangles.append(triangle)
+                    # Remove vertex from vertices
+                    vertices.pop(index)
+                    # Increment triangles found
+                    triangles_found += 1
+                    break
+
     return final_triangles
